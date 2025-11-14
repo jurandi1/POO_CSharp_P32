a5e6fbe57cc6c0b612cb663c4caa86a19182991b
Jurandi Junior <jurandijr20@gmail.com>
2025-11-13 21:22:42 -0300

    Introdução ao LINQ

--- Program.cs
+++ Program.cs
@@ -2,5 +2,18 @@
 {
     private static void Main(string[] args)
     {
+        // Specify the data source.
+        int[] numbers = new int[] { 2, 3, 4, 5 };
+
+        // Define the query expression.
+        IEnumerable<int> result = numbers
+        .Where(x => x % 2 == 0)
+        .Select(x => 10 * x);
+
+        // Execute the query.
+        foreach (int x in result)
+        {
+            Console.WriteLine(x);
+        }
     }
 }
