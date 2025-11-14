2b7f56c1b59d93bc5f9e3aeddc1d3301b33e92d1
Jurandi Junior <jurandijr20@gmail.com>
2025-11-13 21:31:11 -0300

    Entities

---
+++ Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace POO_CSharp_P32.Entities
+{
+    internal class Category
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public int Tier { get; set; }
+    }
+}
